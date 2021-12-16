a4640ab126bcbf37d8f97cd6836a7b2e926c38b6
Sefa Aydınelli <sefa.aydinelli@bestcloudfor.me>
2021-12-16 20:59:11 +0300

    default region changed to eu-central-1

--- ssmctl.sh
+++ ssmctl.sh
@@ -4,8 +4,8 @@
 if [ -z $1 ]
 then
     echo "AWS SSM Session Connector"
-    read -p "Enter the region [eu-west-1] : " aws_region
-    aws_region=${aws_region:-eu-west-1}
+    read -p "Enter the region [eu-central-1] : " aws_region
+    aws_region=${aws_region:-eu-central-1}
     aws ec2 describe-instances --region $aws_region --query 'Reservations[].Instances[].[Tags[?Key==`Name`]| [0].Value,InstanceId,State.Name]' --output table | sort -n
     echo "+----------------------------------+-----------------------+----------+"
     read -p "Copy the ID of the instance : " instance_id
@@ -16,8 +16,8 @@ then
     echo "Help section. WIP"
 else
     echo "AWS SSM Session Connector"
-    read -p "Enter the region [eu-west-1] : " aws_region
-    aws_region=${aws_region:-eu-west-1}
+    read -p "Enter the region [eu-central-1] : " aws_region
+    aws_region=${aws_region:-eu-central-1}
     echo "Connecting to $1 on $aws_region with SSM";
     aws ssm start-session --region $aws_region --target $1
 fi
